777e86d5709c22629064439e17c71836150c3915
Zhavior <32025288+Zhavior@users.noreply.github.com>
2026-07-01 09:21:14 -0300

    Remove legacy global theme bootstraps

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,12 @@
-import './styles/vouchedge-theme.css';
-import './styles/theme-overrides.css';
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { AppErrorBoundary } from './components/system/AppErrorBoundary';
-import { bootVouchEdgeTheme } from "./lib/themeEngine";
 import { forceFounderPoints } from "./lib/founderAccess";
-import { loadSavedTheme } from './theme/themeEngine';
-loadSavedTheme();
 
 forceFounderPoints();
 
-bootVouchEdgeTheme();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
